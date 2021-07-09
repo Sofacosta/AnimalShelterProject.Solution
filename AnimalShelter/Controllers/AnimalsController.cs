d3cab10ff7ef8d4fce49e4e1f8702d6adfaa4043
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-07-08 20:36:55 -0500

    add swagger

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -20,7 +20,6 @@ namespace AnimalShelter.Controllers
       _db = db;
     }
 
-    // GET: api/Animals
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Animal>>> Get(string species, string gender, string name)
     {
@@ -40,7 +39,6 @@ namespace AnimalShelter.Controllers
        return await query.ToListAsync();
     }
 
-    // GET: api/Animals/5
     [HttpGet("{id}")]
     public async Task<ActionResult<Animal>> GetAnimal(int id)
     {
@@ -54,8 +52,6 @@ namespace AnimalShelter.Controllers
       return animal;
     }
 
-    // PUT: api/Animals/5
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPut("{id}")]
     public async Task<IActionResult> PutAnimal(int id, Animal animal)
     {
@@ -85,8 +81,6 @@ namespace AnimalShelter.Controllers
       return NoContent();
     }
 
-    // POST: api/Animals
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost]
     public async Task<ActionResult<Animal>> Post(Animal animal)
     {
@@ -96,7 +90,6 @@ namespace AnimalShelter.Controllers
       return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
     }
 
-    // DELETE: api/Animals/5
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteAnimal(int id)
     {
@@ -118,12 +111,3 @@ namespace AnimalShelter.Controllers
     }
   }
 }
-// return NoContent();
-//         }
-
-//         private bool AnimalExists(int id)
-// {
-//   return _db.Animals.Any(e => e.AnimalId == id);
-// }
-//     }
-//   }
--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
+using Microsoft.OpenApi.Models;
 
 namespace AnimalShelter
 {
@@ -25,6 +26,10 @@ namespace AnimalShelter
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelter", Version ="v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +38,8 @@ namespace AnimalShelter
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
             }
 
             // app.UseHttpsRedirection();
